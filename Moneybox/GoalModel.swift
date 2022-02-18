f94cd95d2a08713358606d52b3c1011b1dd3ea4c
NickSagan <9952728@gmail.com>
2022-02-18 17:13:13 +0300

    add GoalModel

---
+++ Moneybox/GoalModel.swift
@@ -0,0 +1,16 @@
+//
+//  GoalModel.swift
+//  Moneybox
+//
+//  Created by Nick Sagan on 18.02.2022.
+//
+
+import UIKit
+
+struct GoalModel {
+    var name: String
+    var photo: UIImage
+    var price: Int
+    var savings: Int
+    var income: Int
+}
