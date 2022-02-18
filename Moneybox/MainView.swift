a4f2968aafc829f877a02502ba2202a064244945
NickSagan <9952728@gmail.com>
2022-02-18 19:08:53 +0300

    add all views

--- Moneybox/MainView.swift
+++ Moneybox/MainView.swift
@@ -12,14 +12,14 @@ class MainView: UIView {
     
     let goalImage: UIImageView = {
         let img = UIImageView()
-        img.image = UIImage(named: "addGoalImage")
+        img.image = UIImage(named: "piggy")
         img.contentMode = .scaleAspectFit
         return img
     }()
     
     let goalName: UILabel = {
         let label = UILabel()
-        label.text = "Цель:"
+        label.text = "Цель: конструктор Lego"
         label.font = UIFont.boldSystemFont(ofSize: 22)
         label.textAlignment = .left
         label.frame.size.height = 40
@@ -29,28 +29,55 @@ class MainView: UIView {
     let weeksLeft: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
-        label.text = "Осталось копить:\n10 недель \nдо 8 августа 2022"
-        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.text = "Осталось копить\nещё 10 недель \nдо 8 августа 2022"
+        label.font = UIFont.systemFont(ofSize: 17)
         label.textAlignment = .center
         label.frame.size.height = 100
         return label
     }()
     
-    let progressBar: BarProgressView = {
-       let pr = BarProgressView()
-        pr.progress = 0.5
-        pr.progressColor = .systemGreen
-        pr.showTriangle = true
-        return pr
+    let progressBar: GradientHorizontalProgressBar = {
+        let pv = GradientHorizontalProgressBar()
+        pv.progress = 0.6
+        pv.color = UIColor.systemGreen
+        pv.gradientColor = UIColor.white
+        pv.backgroundColor = UIColor.systemGray4
+        return pv
     }()
 
     let moneyLeft: UILabel = {
         let label = UILabel()
         label.text = "Накопи еще 832 рублей"
-        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.font = UIFont.systemFont(ofSize: 17)
         label.textAlignment = .center
         label.frame.size.height = 30
         return label
+    }()
+    
+    let progresslabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Накоплено 532 из 950 рублей"
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textAlignment = .center
+        label.frame.size.height = 30
+        return label
+    }()
+    
+    let plusButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setBackgroundImage(UIImage(systemName: "plus.circle"), for: .normal)
+        btn.frame.size.height = 50
+        btn.frame.size.width = 50
+        return btn
+    }()
+    
+    let minusButton: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setBackgroundImage(UIImage(systemName: "minus.circle"), for: .normal)
+        btn.frame.size.height = 50
+        btn.frame.size.width = 50
+        return btn
     }()
     
     required init?(coder: NSCoder) {
@@ -74,12 +101,16 @@ class MainView: UIView {
         self.addSubview(weeksLeft)
         self.addSubview(progressBar)
         self.addSubview(moneyLeft)
+        progressBar.addSubview(progresslabel)
+        self.addSubview(plusButton)
+        self.addSubview(minusButton)
         
         goalName.translatesAutoresizingMaskIntoConstraints = false
         goalImage.translatesAutoresizingMaskIntoConstraints = false
         weeksLeft.translatesAutoresizingMaskIntoConstraints = false
         progressBar.translatesAutoresizingMaskIntoConstraints = false
         moneyLeft.translatesAutoresizingMaskIntoConstraints = false
+        progresslabel.translatesAutoresizingMaskIntoConstraints = false
     }
     
 //MARK: - SETUP CONSTRAINTS
@@ -105,21 +136,41 @@ class MainView: UIView {
         
         progressBar.snp.makeConstraints { make in
             make.top.equalTo(weeksLeft.snp.bottom).offset(20)
-            make.left.equalTo(goalImage.snp.left)
-            make.right.equalTo(goalImage.snp.right)
+            make.height.equalTo(self).multipliedBy(0.1)
+        }
+        
+        plusButton.snp.makeConstraints { make in
+            make.centerY.equalTo(progressBar.snp.centerY)
+            make.right.equalTo(self).offset(-10)
+            make.left.equalTo(progressBar.snp.right).offset(10)
+            make.height.equalTo(progressBar.snp.height).multipliedBy(0.5)
+            make.width.equalTo(plusButton.snp.height)
+        }
+        
+        minusButton.snp.makeConstraints { make in
+            make.centerY.equalTo(progressBar.snp.centerY)
+            make.left.equalTo(self).offset(10)
+            make.right.equalTo(progressBar.snp.left).offset(-10)
+            make.height.equalTo(progressBar.snp.height).multipliedBy(0.5)
+            make.width.equalTo(plusButton.snp.height)
         }
         
         moneyLeft.snp.makeConstraints { make in
             make.top.equalTo(progressBar.snp.bottom).offset(20)
             make.centerX.equalTo(goalImage.snp.centerX)
         }
+        
+        progresslabel.snp.makeConstraints { make in
+            make.centerX.equalTo(progressBar.snp.centerX)
+            make.centerY.equalTo(progressBar.snp.centerY)
+        }
     }
     
 //MARK: - APPEARANCE
     
     func setAppearance() {
-        if traitCollection.userInterfaceStyle == .dark {
-            self.backgroundColor = .black
-        } else { self.backgroundColor = .white }
+//        if traitCollection.userInterfaceStyle == .dark {
+//            self.backgroundColor = .black
+//        } else { self.backgroundColor = .white }
     }
 }
