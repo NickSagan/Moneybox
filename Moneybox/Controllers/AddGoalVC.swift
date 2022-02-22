c1cae10b037ae2cba270f4e8e1cfdafb374355c6
NickSagan <9952728@gmail.com>
2022-02-22 19:49:39 +0300

    add view and constraints

--- Moneybox/Controllers/AddGoalVC.swift
+++ Moneybox/Controllers/AddGoalVC.swift
@@ -8,5 +8,23 @@
 import UIKit
 
 class AddGoalVC: UIViewController {
+    var addGoalView: AddGoalView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+        addGoalView = AddGoalView()
+        addGoalView.backgroundColor = .systemBackground
+        view.addSubview(addGoalView)
+        addGoalView.translatesAutoresizingMaskIntoConstraints = false
+        addGoalView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide).inset(UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+        }
+
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done , target: self, action: #selector(shareGoal))
+//
+//        goal = GoalModel(name: "Lego NinjaGo", photo: UIImage(named: "piggy")!, price: 9000, savings: 3400, income: 1000)
+//
+//        refreshGoal()
+    }
 }
