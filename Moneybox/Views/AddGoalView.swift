53b23c7bc800662abc18f4099a96cf647e962a29
NickSagan <9952728@gmail.com>
2022-02-24 19:22:19 +0300

    apply KaedeTextField add error label

--- Moneybox/Views/AddGoalView.swift
+++ Moneybox/Views/AddGoalView.swift
@@ -19,12 +19,12 @@ class AddGoalView: UIView {
         return label
     }()
     
-    let nameInput: TextField = {
-        let tf = TextField()
-        tf.placeholder = " На что вы копите?"
+    let nameInput: KaedeTextField = {
+        let tf = KaedeTextField()
+        tf.placeholder = "Название"
         tf.font = UIFont.systemFont(ofSize: 17)
         tf.textAlignment = .left
-        tf.frame.size.height = 30
+        tf.frame.size.height = 40
         tf.layer.borderWidth = 1
         tf.layer.borderColor = UIColor.systemGray.cgColor
         tf.layer.cornerRadius = 8
@@ -32,46 +32,58 @@ class AddGoalView: UIView {
         return tf
     }()
     
-    let priceInput: TextField = {
-        let tf = TextField()
-        tf.placeholder = " Сколько нужно накопить?"
+    let priceInput: KaedeTextField = {
+        let tf = KaedeTextField()
+        tf.placeholder = "Стоимость"
         tf.font = UIFont.systemFont(ofSize: 17)
         tf.textAlignment = .left
-        tf.frame.size.height = 30
+        tf.frame.size.height = 40
         tf.layer.borderWidth = 1
         tf.layer.borderColor = UIColor.systemGray.cgColor
         tf.layer.cornerRadius = 8
-        tf.keyboardType = .decimalPad
+        tf.keyboardType = .numberPad
         tf.returnKeyType = .done
         return tf
     }()
     
-    let savingsInput: TextField = {
-        let tf = TextField()
-        tf.placeholder = " Сколько уже накоплено?"
+    let savingsInput: KaedeTextField = {
+        let tf = KaedeTextField()
+        tf.placeholder = "Уже накоплено"
         tf.font = UIFont.systemFont(ofSize: 17)
         tf.textAlignment = .left
-        tf.frame.size.height = 30
+        tf.frame.size.height = 40
         tf.layer.borderWidth = 1
         tf.layer.borderColor = UIColor.systemGray.cgColor
         tf.layer.cornerRadius = 8
-        tf.keyboardType = .decimalPad
+        tf.keyboardType = .numberPad
         tf.returnKeyType = .done
         return tf
     }()
     
-    let incomeInput: TextField = {
-        let tf = TextField()
-        tf.placeholder = " Сколько вы получаете в неделю?"
+    let incomeInput: KaedeTextField = {
+        let tf = KaedeTextField()
+        tf.placeholder = "Доход в неделю"
         tf.font = UIFont.systemFont(ofSize: 17)
         tf.textAlignment = .left
-        tf.frame.size.height = 30
+        tf.frame.size.height = 40
         tf.layer.borderWidth = 1
         tf.layer.borderColor = UIColor.systemGray.cgColor
         tf.layer.cornerRadius = 8
-        tf.keyboardType = .decimalPad
+        tf.keyboardType = .numberPad
         tf.returnKeyType = .done
         return tf
+    }()
+    
+    let errorLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = UIColor.systemRed
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textAlignment = .left
+        label.frame.size.height = 80
+        label.numberOfLines = 0
+        label.lineBreakMode = .byClipping
+        return label
     }()
     
     required init?(coder: NSCoder) {
@@ -95,12 +107,14 @@ class AddGoalView: UIView {
         self.addSubview(priceInput)
         self.addSubview(savingsInput)
         self.addSubview(incomeInput)
+        self.addSubview(errorLabel)
         
         header.translatesAutoresizingMaskIntoConstraints = false
         nameInput.translatesAutoresizingMaskIntoConstraints = false
         priceInput.translatesAutoresizingMaskIntoConstraints = false
         savingsInput.translatesAutoresizingMaskIntoConstraints = false
         incomeInput.translatesAutoresizingMaskIntoConstraints = false
+        errorLabel.translatesAutoresizingMaskIntoConstraints = false
     }
     
 //MARK: - SETUP CONSTRAINTS
@@ -117,28 +131,35 @@ class AddGoalView: UIView {
             make.top.equalTo(header.snp.bottom).offset(20)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(30)
+            make.height.equalTo(40)
         }
         
         priceInput.snp.makeConstraints { make in
             make.top.equalTo(nameInput.snp.bottom).offset(20)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(30)
+            make.height.equalTo(40)
         }
         
         savingsInput.snp.makeConstraints { make in
             make.top.equalTo(priceInput.snp.bottom).offset(20)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(30)
+            make.height.equalTo(40)
         }
         
         incomeInput.snp.makeConstraints { make in
             make.top.equalTo(savingsInput.snp.bottom).offset(20)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(30)
+            make.height.equalTo(40)
+        }
+        
+        errorLabel.snp.makeConstraints { make in
+            make.top.equalTo(incomeInput.snp.bottom).offset(15)
+            make.left.equalTo(self).offset(20)
+            make.right.equalTo(self).offset(-20)
+            make.height.equalTo(80)
         }
 
     }
