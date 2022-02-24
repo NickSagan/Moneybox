3698044854050e51727ef7b54df5116e7cbcf35b
NickSagan <9952728@gmail.com>
2022-02-24 20:50:47 +0300

    fix CongratsView

--- Moneybox/Views/CongratsView.swift
+++ Moneybox/Views/CongratsView.swift
@@ -13,16 +13,18 @@ class CongratsView: UIView {
     let title: UILabel = {
         let label = UILabel()
         label.text = "Поздравляем!"
-        label.font = UIFont.systemFont(ofSize: 48)
+        label.font = UIFont.systemFont(ofSize: 36)
+        label.textColor = .white
         label.textAlignment = .center
-        label.frame.size.height = 70
+        label.frame.size.height = 40
         return label
     }()
     
     let subTitle: UILabel = {
         let label = UILabel()
-        label.text = "Вы достигли вашей цели! Вы накопили 500000 рублей.\n\nВаша цель: машинка на пульте упарвления"
-        label.font = UIFont.systemFont(ofSize: 24)
+        label.text = "Вы достигли вашей цели! Вы накопили 500000 рублей"
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.textColor = .white
         label.textAlignment = .center
         label.numberOfLines = 10
         label.lineBreakMode = .byClipping
@@ -32,23 +34,18 @@ class CongratsView: UIView {
     let newGoalButton: UIButton = {
         let btn = UIButton()
         btn.setTitle("Задать новую цель", for: .normal)
-        return btn
-    }()
-    
-    let laterButton: UIButton = {
-        let btn = UIButton()
-        btn.setTitle("Пропустить", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
         return btn
     }()
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
-        self.backgroundColor = UIColor.white.withAlphaComponent(0)
+        self.backgroundColor = UIColor.black.withAlphaComponent(0)
     }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = UIColor.white.withAlphaComponent(0)
+        self.backgroundColor = UIColor.black.withAlphaComponent(0)
         addSubviews()
         setupConstraints()
         setAppearance()
@@ -60,43 +57,34 @@ class CongratsView: UIView {
         self.addSubview(title)
         self.addSubview(subTitle)
         self.addSubview(newGoalButton)
-        self.addSubview(laterButton)
         
         title.translatesAutoresizingMaskIntoConstraints = false
         subTitle.translatesAutoresizingMaskIntoConstraints = false
         newGoalButton.translatesAutoresizingMaskIntoConstraints = false
-        laterButton.translatesAutoresizingMaskIntoConstraints = false
     }
     
 //MARK: - SETUP CONSTRAINTS
     
     func setupConstraints() {
         title.snp.makeConstraints { make in
-            make.top.equalTo(self).offset(20)
+            make.top.equalTo(self).offset(0)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(70)
+            make.height.equalTo(40)
         }
         
         subTitle.snp.makeConstraints { make in
-            make.top.equalTo(title.snp.bottom).offset(40)
+            make.top.equalTo(title.snp.bottom).offset(10)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(300)
+            make.height.equalTo(100)
         }
         
         newGoalButton.snp.makeConstraints { make in
-            make.top.equalTo(subTitle.snp.bottom).offset(20)
+            make.top.equalTo(subTitle.snp.bottom).offset(10)
             make.left.equalTo(self).offset(20)
             make.right.equalTo(self).offset(-20)
-            make.height.equalTo(40)
-        }
-        
-        laterButton.snp.makeConstraints { make in
-            make.top.equalTo(newGoalButton.snp.bottom).offset(40)
-            make.left.equalTo(self).offset(20)
-            make.right.equalTo(self).offset(-20)
-            make.height.equalTo(40)
+            make.height.equalTo(20)
         }
     }
     
