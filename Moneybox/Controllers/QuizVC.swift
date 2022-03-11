bf9c28417ed58b198f13a39e472aa4a9cdbfa4ed
NickSagan <9952728@gmail.com>
2022-03-11 20:50:15 +0300

    start QuizVC

--- Moneybox/Controllers/QuizVC.swift
+++ Moneybox/Controllers/QuizVC.swift
@@ -9,4 +9,22 @@ import UIKit
 
 class QuizVC: UIViewController {
     
+    var quizView: QuizView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    func setupUI() {
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "mainBg")!)
+        quizView = QuizView()
+        view.addSubview(quizView)
+        quizView.translatesAutoresizingMaskIntoConstraints = false
+        quizView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide).inset(UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+        }
+        
+
+    }
 }
