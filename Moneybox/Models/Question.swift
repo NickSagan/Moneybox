3d874199a9bcd531e7c0b3609ee42d08658d6f2d
NickSagan <9952728@gmail.com>
2022-03-13 17:55:28 +0300

    add struct Question

---
+++ Moneybox/Models/Question.swift
@@ -0,0 +1,16 @@
+//
+//  Question.swift
+//  Моя копилка сбережений
+//
+//  Created by Nick Sagan on 13.03.2022.
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let answer1: String
+    let answer2: String
+    let answer3: String
+    let correctAnswer: Int
+}
