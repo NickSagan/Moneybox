5cd6664407d3814c4ae8b8eea683f3a7b2de49d3
NickSagan <9952728@gmail.com>
2022-03-13 20:51:15 +0300

    add onboarding in scene delegate

--- Moneybox/SceneDelegate.swift
+++ Moneybox/SceneDelegate.swift
@@ -19,18 +19,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-//        if (UserDefaults.standard.bool(forKey: "notFirstLaunch") == false) {
-//            UserDefaults.standard.set(true, forKey: "notFirstLaunch")
-//            window?.rootViewController = OnboardVC()
-//        } else {
-//            let nc = UINavigationController(rootViewController: CalendarVC())
-//            nc.navigationBar.backgroundColor = .systemBackground
-//            window?.rootViewController = nc
-//        }
-        let nc = UINavigationController(rootViewController: MainVC())
-        nc.navigationBar.backgroundColor = UIColor.white.withAlphaComponent(0)
-        nc.navigationBar.tintColor = .white
-        window?.rootViewController = nc
+        if (UserDefaults.standard.bool(forKey: "notFirstLaunch") == false) {
+            UserDefaults.standard.set(true, forKey: "notFirstLaunch")
+            window?.rootViewController = OnboardVC()
+        } else {
+            let nc = UINavigationController(rootViewController: MainVC())
+            nc.navigationBar.backgroundColor = UIColor.white.withAlphaComponent(0)
+            nc.navigationBar.tintColor = .white
+            window?.rootViewController = nc
+        }
+        
         window?.backgroundColor = .systemBackground
         window?.makeKeyAndVisible()
     }
